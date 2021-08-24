30d6adfc40699f83407550f85e4293a499856985
BAR-KEY <park96h@naver.com>
2021-08-24 18:52:14 +0900

    make template

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import TodoListTemplate from "./components/TodoListTemplate";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>BARKEY TODO</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoListTemplate form={"제목"} children={"내용"}>
+        템플릿!
+      </TodoListTemplate>
     </div>
   );
 }
